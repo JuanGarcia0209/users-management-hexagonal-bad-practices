ed84fdde535dfffbbfe9f8f798b154d1241246d6
Juan David Navarro Garcia <159143340+JuanGarcia0209@users.noreply.github.com>
2026-05-26 03:59:52 -0500

    refactor: Clean Code - Regla 10 - extraer texto hardcode a constante en UserNotFoundException (mantener comentario)

--- src/main/java/com/jcaa/usersmanagement/domain/exception/UserNotFoundException.java
+++ src/main/java/com/jcaa/usersmanagement/domain/exception/UserNotFoundException.java
@@ -6,9 +6,11 @@ public final class UserNotFoundException extends DomainException {
     super(message);
   }
 
+  private static final String USER_NOT_FOUND_FORMAT = "The user with id '%s' was not found.";
+
   public static UserNotFoundException becauseIdWasNotFound(final String userId) {
     // VIOLACIÓN Regla 10: texto de error hardcodeado directamente en el método fábrica.
     // Debe usarse una constante con nombre descriptivo en lugar de un String literal.
-    return new UserNotFoundException(String.format("The user with id '%s' was not found.", userId));
+    return new UserNotFoundException(String.format(USER_NOT_FOUND_FORMAT, userId));
   }
 }
